1fe2813026fc589f626be8453cedd043e0806901
vineethpaul-1997 <ervineethpv137@gmail.com>
2026-06-11 02:13:17 +0000

    new code for app.py

--- app.py
+++ app.py
@@ -2,16 +2,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template('index.html')
 
-@app.route("/health")
-def health():
-    return {
-        "status": "Running",
-        "application": "Python DevOps Demo"
-    }
+@app.route('/python.html')
+def python():
+    return render_template('python.html')
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+@app.route('/docker.html')
+def docker():
+    return render_template('docker.html')
+
+@app.route('/jenkins.html')
+def jenkins():
+    return render_template('jenkins.html')
+
+@app.route('/kubernetes.html')
+def kubernetes():
+    return render_template('kubernetes.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
